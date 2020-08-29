fe7aced92b54418849b79b4a78c5245cc510a75d
ysherqawi <ysherqawi@gmail.com>
2020-08-29 16:06:48 +0300

    installed and setup socketio

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const publicDirectoryPath = path.join(__dirname, '../public');
 app.use(express.static(publicDirectoryPath));
 
+io.on('connection', () => {
+  console.log('New WebSocket Connection');
+});
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is Up And Running on Port ${PORT}`);
 });
