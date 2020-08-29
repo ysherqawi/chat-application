c92ccf69c3cb303d518c339d78c0ad7fbaf56d5b
ysherqawi <ysherqawi@gmail.com>
2020-08-29 22:28:45 +0300

    added broadcasting a message when a user is join or leave

--- src/index.js
+++ src/index.js
@@ -14,9 +14,14 @@ io.on('connection', (socket) => {
   console.log('New WebSocket Connection');
 
   socket.emit('message', 'Welcome !');
+  socket.broadcast.emit('message', 'A new user has joined !');
 
   socket.on('sendMessage', (data) => {
     io.emit('message', data);
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left !');
   });
 });
 
