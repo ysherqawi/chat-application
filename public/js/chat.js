da860ead58dc3381330cf10e35e18285bac65d81
ysherqawi <ysherqawi@gmail.com>
2020-08-30 14:41:02 +0300

    rendering messages in the ui

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,9 +4,14 @@ const $messageForm = document.querySelector('#message-form');
 const $messageFormInput = $messageForm.querySelector('input');
 const $messageFormButton = $messageForm.querySelector('button');
 const $sendLocationButton = document.querySelector('#send-location');
+const $messages = document.querySelector('#messages');
+
+const messageTemplate = document.querySelector('#message-template').innerHTML;
 
 socket.on('message', (message) => {
   console.log(message);
+  const html = Mustache.render(messageTemplate, { message });
+  $messages.insertAdjacentHTML('beforeend', html);
 });
 
 $messageForm.addEventListener('submit', (e) => {
